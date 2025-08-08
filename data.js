7460800b0a802d6a6ec58b8a499bb3e45a2ce58b
iLemonOG <chrswkrt@gmail.com>
2025-08-08 10:43:43 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+// Events per second (baseline)
+const eventRates = [
+  { name: "Star Birth", value: 1.8 },
+  { name: "Star Quiet Fade", value: 1.2 },
+  { name: "Star Catastrophic Death", value: 0.0001 },
+  { name: "Planet Birth", value: 5 },
+  { name: "Planet Displaced", value: 0.02 },
+  { name: "Planet Catastrophic Death", value: 0.0005 }
+];
